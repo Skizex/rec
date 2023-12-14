ddd14ae5e9649299adc3396957d7687bbd3c8026
Skizex <justskizex@gmail.com>
2023-12-14 16:05:35 +0400

    fail

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadInt(string massage)
+{
+    Console.WriteLine(massage);
+    return int.Parse(Console.ReadLine());
+}
+
+
+int number = ReadInt("Введите число N");
+PrintNumbers(number);
+
+
+void PrintNumbers(int n);
+{
+    if(n == 1)
+        return;
+   
+    Console.WriteLine(n + " ");
+    PrintNumbers(n-1);
+}
